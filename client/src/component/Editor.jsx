ceb291bc0b260b28497a9f38f380a33486c2095c
Rakesh <rakeshabde@gmail.com>
2022-12-15 19:15:11 +0530

    Extra fonts added

--- client/src/component/Editor.jsx
+++ client/src/component/Editor.jsx
@@ -8,10 +8,35 @@ import styled from '@emotion/styled';
 
 import { io } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
-//hello worlld
+
 const Component = styled.div`
 background : #F5F5F5
 `
+
+{
+// specify the fonts you would 
+var fonts = ['Arial', 'Courier', 'Garamond', 'Tahoma', 'Times New Roman', 'Verdana', 'Impact', 'Futura','Bodoni'];
+// generate code friendly names
+function getFontName(font) {
+    return font.toLowerCase().replace(/\s/g, "-");
+}
+var fontNames = fonts.map(font => getFontName(font));
+// add fonts to style
+var fontStyles = "";
+fonts.forEach(function(font) {
+    var fontName = getFontName(font);
+    fontStyles += ".ql-snow .ql-picker.ql-font .ql-picker-label[data-value=" + fontName + "]::before, .ql-snow .ql-picker.ql-font .ql-picker-item[data-value=" + fontName + "]::before {" +
+        "content: '" + font + "';" +
+        "font-family: '" + font + "', sans-serif;" +
+        "}" +
+        ".ql-font-" + fontName + "{" +
+        " font-family: '" + font + "', sans-serif;" +
+        "}";
+});
+var node = document.createElement('style');
+node.innerHTML = fontStyles;
+document.body.appendChild(node);
+}
 
 const toolbarOptions = [
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
@@ -27,13 +52,24 @@ const toolbarOptions = [
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
   
     [{ 'color': [] }, { 'background': [] }, 'image','video','link'],          // dropdown with defaults from theme
-    [{ 'font': [] }],
+    [{ 'font': fontNames }],
     [{ 'align': [] }],
   
     ['clean']                                         // remove formatting button
   ];
   
- 
+ // Add fonts to whitelist
+var Font = Quill.import('formats/font');
+Font.whitelist = fontNames;
+Quill.register(Font, true);
+
+var quill = new Quill('#editor', {
+    modules: {
+        toolbar: toolbarOptions
+    },
+    theme: 'snow'
+});
+
 const Editor = () => {
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
